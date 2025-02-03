240d45672d4bd1ca3fb1eea46297e6a6bbe3352f
m <jasperbos1234@gmail.com>
2025-02-03 23:05:11 +0100

    Update website metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,9 +28,42 @@ const Unbound = Unbounded({
   weight: "400",
 });
 
+// export const metadata: Metadata = {
+//   title: "dumbass.dog 🐾🦴",
+//   description: "Silly programmer nerd, Counter-Strike enthusiast and Monster Ultra fueled dog.",
+// };
+
 export const metadata: Metadata = {
   title: "dumbass.dog 🐾🦴",
   description: "Silly programmer nerd, Counter-Strike enthusiast and Monster Ultra fueled dog.",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    type: "website",
+    url: "https://dumbass.dog",
+    title: "dumbass.dog 🐾🦴",
+    description: "Silly programmer nerd, Counter-Strike enthusiast and Monster Ultra fueled dog.",
+    images: [
+      {
+        url: "https://avatars.githubusercontent.com/u/96448965",
+        width: 400,
+        height: 400,
+        alt: "dumbass.dog icon",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary",
+    site: "@femd0g",
+    title: "dumbass.dog 🐾🦴",
+    description: "Silly programmer nerd, Counter-Strike enthusiast and Monster Ultra fueled dog.",
+    images: "https://avatars.githubusercontent.com/u/96448965",
+  },
+};
+
+export const viewport = {
+  themeColor: "#0c0c0c",
 };
 
 export default function RootLayout({
