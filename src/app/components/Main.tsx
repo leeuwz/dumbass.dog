37685cb549674c2c0d93ae4e24401b27b270fd74
m <Jasperbos1234@gmail.com>
2025-07-13 14:31:53 +0200

    revise main text

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -10,8 +10,8 @@ export default function Main() {
                 <div>
                     <p className="text-xl font-[family-name:var(--font-sour-gummy)]">
                     My name is lionz or dvmbassdog, hope you are doing well!<br/>
-                    I&apos;m mainly known for clicking circles, striking the counter and being a silly dawg I guess..<br/>
-                    Oh yeah! I&apos;m also a nerdy programmer, you should check some of my stuff out below!!<br/>
+                    I&apos;m mainly known for clicking circles, striking the counter and barking at other dogs<br/>
+                    Oh yeah! I also like to code sometimes, you should totally go check it out and stuff...<br/>
                     Other then that I like to work on
                     <a className="relative underline text-[#f97af8] hover:text-[#ff00ff] transition ml-1 
                     before:content-[''] before:absolute before:left-0 before:bottom-0 before:w-0 before:h-full 
