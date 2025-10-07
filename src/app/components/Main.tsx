d8e4b5526a339adaa37bb2cbb25298af97564285
m <Jasperbos1234@gmail.com>
2025-10-07 19:04:33 +0200

    update Main.tsx text

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -9,15 +9,15 @@ export default function Main() {
             <div className="flex flex-col gap-10 xl:gap-0 xl:flex-row justify-between items-start xl:items-center w-full mt-4">
                 <div>
                     <p className="text-xl font-[family-name:var(--font-sour-gummy)]">
-                    My name is lionz or dvmbassdog, hope you are doing well!<br/>
-                    I&apos;m mainly known for clicking circles, striking the counter and barking at other dogs<br/>
-                    Oh yeah! I also like to code sometimes, you should totally go check it out and stuff...<br/>
-                    Other then that I like to work on
+                    i'm lionz, hope you are doing well<br/>
+                    mostly busy clicking circles, striking the counter or barking at other dogs<br/>
+                    I also like to code sometimes, you should totally go check it or smth..<br/>
+                    sometimes i also work on
                     <a className="relative underline text-[#f97af8] hover:text-[#ff00ff] transition ml-1 
                     before:content-[''] before:absolute before:left-0 before:bottom-0 before:w-0 before:h-full 
                     before:bg-[#f97af8] before:transition-all before:duration-300 hover:before:w-full hover:before:z-[-1]"
-                    href="https://github.com/osukomako" target="_blank">komako</a> sometimes or make silly websites like this one.<br/>
-                    Thanks for stopping by!!! ^w^
+                    href="https://github.com/osukomako" target="_blank">komako</a> or make silly websites like this one<br/>
+                    thanks for stopping by &gt;w&lt;
                     </p>
                 </div>
                 <Discord />
